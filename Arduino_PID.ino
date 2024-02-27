4b389c9b77906963120374fa7fd3ec6645731ade
BrandonRuf <91632371+BrandonRuf@users.noreply.github.com>
2024-02-27 14:00:22 -0500

    Added comments

--- Arduino_PID.ino
+++ Arduino_PID.ino
@@ -1,9 +1,9 @@
 /*
- * Example script for use in the PHYS 339 PID lab at McGill University.
+ * Example script for use in the PHYS-339 PID lab at McGill University.
  * Written by Brandon Ruffolo in Feb 2024.
  */
 
-#include "Adafruit_MAX31856.h"
+#include "Adafruit_MAX31856.h" // Make sure to install this library!
 
 #define SKETCH_VERSION "0.0.1"
 #define BAUD 115200 
@@ -11,7 +11,7 @@
 Adafruit_MAX31856 thermocouple = Adafruit_MAX31856(5,4,3,2); // Use software SPI: CS, DI, DO, CLK
 
 uint32_t t0 = 0;         // Reference time
-uint16_t heater = 62499; // Heater power
+uint16_t heater = 8000;  // Heater power
 
 void setup() {
   Serial.begin(BAUD);      // Enable Serial COM
@@ -28,19 +28,19 @@ void setup() {
   ICR1 = 62499;                       // Set the PWM frequency to 1Hz: 16MHz/(256 * 1Hz) - 1 = 62499
   OCR1A = 0;                          // Output comapre register for setting duty cycle (This can be conveniently set with the basic analogRead() function)
   
-  t0 = millis(); // Set start time 
-  analogWrite(9,heater); 
+  t0 = millis();         // Set start time 
+  analogWrite(9,heater); // Set a fixed heater power
 }
 
 void loop() {
 
   float temperature = thermocouple.readThermocoupleTemperature(); // Read the last temperature measurement made by the thermocouple
-  delay(100);
+  delay(100); // 100 ms delay to give the MAX31856 time for next temperature conversion (see datasheet!)
 
   /* Print out time, temperature, and heater data (comma delimited)*/
   Serial.print(millis()-t0);
   Serial.print(",");
   Serial.print(String(temperature,4));
   Serial.print(",");
-  Serial.println(heater);
+  Serial.println(heater); 
 }
